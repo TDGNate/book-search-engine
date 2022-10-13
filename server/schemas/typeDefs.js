a9abfaaee39052c6e8a3e6a41e42b8399ff43b26
Nate <itsnzte@gmail.com>
2022-10-13 15:34:14 -0700

    update typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,37 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    books: [String]!
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [User]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+
+    addUser(name: String!, email: String!, password: String!): Auth
+
+    saveBook(author: String!, description: String!, title: String!, bookId: ID!, image: String!): User
+
+    removeBook(bookId: ID!): User
+  }
+`;
+
 module.exports = typeDefs;
