231620bc4bbe7284dfdcbcc696e39629c2ad3469
Nate <itsnzte@gmail.com>
2022-10-14 09:08:41 -0700

    update server js file

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// const routes = require('./routes'); 
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -24,15 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -46,4 +39,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
 };
 
+// Start the server 
 startApolloServer(typeDefs, resolvers);
