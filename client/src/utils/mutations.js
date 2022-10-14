0c9de14253c5ddf915667c4b2f564f9814fe5b12
Nate <itsnzte@gmail.com>
2022-10-14 10:17:50 -0700

    updated mutations, added save book and remove book mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,47 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: ID!) {
+    saveBook(bookId: $bookId) {
+      bookId
+      authors
+      description
+      title
+    }
+  }
+`
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    bookId
+    authors
+    description
+    title
+  }
+}
+`
