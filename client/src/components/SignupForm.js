ea61ac74aeab1edc0f887b0881db11df61b2afc1
Nate <itsnzte@gmail.com>
2022-10-15 07:27:40 -0700

    updated signing in forms

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+
+import { ADD_USER } from "../utils/mutations"
+
+
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+
+  const [addUser, { data, error }] = useMutation(ADD_USER);
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -28,7 +35,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
